553b2b74806e8c26ebcadaa99890f9091ac95b30
mahmoud120000564 <mahmoud120000564@cs.modern-academy.edu.eg>
2024-01-20 15:20:21 +0200

    the new function

--- test/test.js
+++ test/test.js
@@ -1,3 +1,7 @@
-function calculateDaysBetweenDates(begin, end) {}
-
-// Calculates the number of days between
+function calculateDaysBetweenDates(begin, end) {
+  const oneDay = 24 * 60 * 60 * 1000; // hours * minutes * seconds * milliseconds
+  const startDate = new Date(begin);
+  const endDate = new Date(end);
+  const diffDays = Math.round(Math.abs((startDate - endDate) / oneDay));
+  return diffDays;
+}
