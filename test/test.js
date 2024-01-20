d8c343c3ede701bf2dbe141566188e12fe4bc325
mahmoud120000564 <mahmoud120000564@cs.modern-academy.edu.eg>
2024-01-20 23:34:08 +0200

    delete test directory

--- test/test.js
+++
@@ -1,7 +0,0 @@
-function calculateDaysBetweenDates(begin, end) {
-  const oneDay = 24 * 60 * 60 * 1000; // hours * minutes * seconds * milliseconds
-  const startDate = new Date(begin);
-  const endDate = new Date(end);
-  const diffDays = Math.round(Math.abs((startDate - endDate) / oneDay));
-  return diffDays;
-}
